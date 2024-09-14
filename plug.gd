1bcd4a31a64124a5eeeffcdc711d4926af8e4e9a
Russell Matney <russell.matney@gmail.com>
2024-09-14 18:44:04 -0400

    deps: add AsepriteWizard!

--- plug.gd
+++ plug.gd
@@ -3,3 +3,4 @@ extends "res://addons/gd-plug/plug.gd"
 func _plugging():
 	plug("ephread/inkgd", {branch="godot4", include=["addons/inkgd"]})
 	plug("russmatney/log.gd", {include=["addons/log"]})
+	plug("viniciusgerevini/godot-aseprite-wizard", {include=["addons/AsepriteWizard"]})
