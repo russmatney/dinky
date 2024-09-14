3b786025d0231bd103bebccee54f500b5814894a
Russell Matney <russell.matney@gmail.com>
2024-09-14 14:17:48 -0400

    deps: add log.gd

--- plug.gd
+++ plug.gd
@@ -1,4 +1,5 @@
 extends "res://addons/gd-plug/plug.gd"
 
 func _plugging():
-	plug("ephread/inkgd", {branch="godot4", inclue=["addons/inkgd"]})
+	plug("ephread/inkgd", {branch="godot4", include=["addons/inkgd"]})
+	plug("russmatney/log.gd", {include=["addons/log"]})
