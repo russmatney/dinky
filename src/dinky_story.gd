1f48248101474dac30da8e5eb1a61c831cee67c4
Russell Matney <russell.matney@gmail.com>
2024-09-14 15:47:08 -0400

    feat: set_image, clear_image tag handlers for background/portrait
    Impls handlers for SetImage and ClearImage on the Background + Portrait
    texture rects.

--- src/dinky_story.gd
+++ src/dinky_story.gd
@@ -2,7 +2,8 @@ extends Node
 
 ## vars ##############################################
 
-@onready var background = $%Background
+@onready var background_texture_rect = $%Background
+@onready var portrait_texture_rect = $%Portrait
 @onready var choices_list = $%ChoicesList
 @onready var speaker_label = $%SpeakerLabel
 @onready var speaker_container = $%SpeakerContainer
@@ -37,7 +38,7 @@ func continued(text, tags):
 	Log.pr("story continued")
 
 	if !tags.is_empty():
-		Log.pr("tags to handle!", tags)
+		handle_tags(tags)
 
 	dialogue_label.text = text
 
@@ -73,3 +74,77 @@ func ended():
 func remove_choices():
 	for ch in choices_list.get_children():
 		ch.queue_free()
+
+## tag handlers ##############################################
+
+var tag_handlers = {
+	"SetImage": set_image,
+	"ClearImage": clear_image,
+	}
+
+func handle_tags(tags):
+	if (tags.is_empty()):
+		Log.warn("Empty tags passed")
+		return
+	for tag in tags:
+		handle_tag(tag)
+
+func handle_tag(tag):
+	var tag_parts = Array(tag.split(" "))
+	if (tag_parts.is_empty()):
+		Log.warn("Empty tags passed (no command?)")
+		return
+
+	var cmd = tag_parts.pop_front()
+	Log.pr("tag", {cmd=cmd, args=tag_parts})
+
+	if cmd in tag_handlers:
+		tag_handlers[cmd].call(tag_parts)
+
+## image handlers ##############################################
+
+var image_refs = {
+	"Background": func(): return background_texture_rect,
+	"Portrait": func(): return portrait_texture_rect,
+	}
+
+func get_image_texture_rect(args):
+	if args.is_empty():
+		Log.warn("empty image args!", args)
+		return
+	var ref = args[0]
+	if ref not in image_refs:
+		Log.warn("ref not handled by image_refs!", args, ref)
+		return
+	return image_refs[ref].call()
+
+func get_image_path(args):
+	if len(args) < 2:
+		Log.warn("expected image args to have path!", args)
+		return
+	var p = str("res://assets/" + args[1] + ".png")
+	if not FileAccess.file_exists(p):
+		Log.warn("No file found at a path", p)
+		return
+	return p
+
+func set_image(args):
+	Log.pr("set_image: ", args)
+	var texture_rect = get_image_texture_rect(args)
+	var path = get_image_path(args)
+	if not texture_rect or not path:
+		Log.warn("Error setting image with args", args)
+		return
+
+	var texture = load(path)
+	texture_rect.set_texture(texture)
+	texture_rect.visible = true;
+
+func clear_image(args):
+	Log.pr("clear_image: ", args)
+
+	var texture_rect = get_image_texture_rect(args)
+	if not texture_rect:
+		Log.warn("Error clearing image with args", args)
+		return
+	texture_rect.visible = false;
